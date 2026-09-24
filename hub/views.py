cd4c1571c36bc6a3cc18d7e0723f1cbbabea90a8
midetc <mikhailetc.artem@gmail.com>
2026-09-24 15:14:51 +0300

    feat: require login for home and add logout

--- hub/views.py
+++ hub/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.http.request import HttpRequest
 from django.shortcuts import render
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
-
     return render(request, 'hub/index.html')
